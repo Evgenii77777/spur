50fa9ae76c7458274e5949ded58f6414feaa5fbd
Evgeniy <epetrovskij35@gmail.com>
2022-08-24 14:19:43 +0300

    fix task-2

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,21 +1,42 @@
 /* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
-const root = {
-  btn: document.querySelector(".collapsible__button"),
-  hidden: document.querySelector(".collapsible__action--hidden"),
-  visible: document.querySelector(".collapsible__action--visible"),
-  content: document.querySelector(".collapsible__content"),
+const ref = {
+  $btn: document.querySelector(".collapsible__button"),
+  $hidden: document.querySelector(".collapsible__action--hidden"),
+  $visible: document.querySelector(".collapsible__action--visible"),
+  $content: document.querySelector(".collapsible__content"),
 };
 
-root.btn.addEventListener("click", () => {
-  if (root.visible.style.display !== "none") {
-    root.visible.style.display = "none";
-    root.hidden.style.display = "inline-block";
-    root.content.style.overflow = "hidden";
-    root.content.style.maxHeight = "0";
-    root.content.style.transition = "max-height 0.3s ease-out";
+(function () {
+  ref.$visible.style.display = "none";
+  ref.$content.style.overflow = "hidden";
+  ref.$content.style.maxHeight = "0";
+})();
+
+const showContent = [
+  { maxHeight: "0" },
+  { maxHeight: ref.$content.scrollHeight + "px" },
+];
+
+const hiddenContent = [
+  { maxHeight: ref.$content.scrollHeight + "px" },
+  { maxHeight: "0" },
+];
+
+const animationTiming = {
+  duration: 500,
+  easing: "ease-out",
+};
+
+ref.$btn.addEventListener("click", () => {
+  if (ref.$visible.style.display !== "none") {
+    ref.$visible.style.display = "none";
+    ref.$hidden.style.display = "inline-block";
+    ref.$content.animate(hiddenContent, animationTiming);
+    ref.$content.style.maxHeight = "0";
   } else {
-    root.visible.style.display = "inline-block";
-    root.hidden.style.display = "none";
-    root.content.style.maxHeight = root.content.scrollHeight + "px";
+    ref.$visible.style.display = "inline-block";
+    ref.$hidden.style.display = "none";
+    ref.$content.animate(showContent, animationTiming);
+    ref.$content.style.maxHeight = ref.$content.scrollHeight + "px";
   }
 });
